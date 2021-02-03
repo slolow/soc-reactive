27dbc61beb978152d37a813dd60490f04cdbac00
slolow <laszlostarost@googlemail.com>
2021-02-03 21:56:23 +0100

    Created DateContollerTest

---
+++ src/test/java/de/unikassel/soc/socreactive/controller/DateControllerTest.java
@@ -0,0 +1,15 @@
+package de.unikassel.soc.socreactive.controller;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateControllerTest {
+
+    @Test
+    void createDateTimeFluxStream() {
+    }
+
+}
